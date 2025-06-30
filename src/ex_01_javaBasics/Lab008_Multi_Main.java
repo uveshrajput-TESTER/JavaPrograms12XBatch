62380122c3cdba7b815ba18f25f533374c3ca30c
uveshrajput-TESTER <uveshrajpute@gmail.com>
2025-06-30 12:09:03 +0530

    30 jun

---
+++ src/ex_01_javaBasics/Lab008_Multi_Main.java
@@ -0,0 +1,11 @@
+package ex_01_javaBasics;
+
+public class Lab008_Multi_Main {
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+
+    public static void main(int args) {
+
+    }
+}
---
+++ src/ex_02_Java_Basics_Part_2/Lab009_Variables.java
@@ -0,0 +1,8 @@
+package ex_02_Java_Basics_Part_2;
+
+public class Lab009_Variables {
+    public static void main(String[] args) {
+        //data_type Variable_name = variable_value
+        byte age = 34;
+    }
+}
